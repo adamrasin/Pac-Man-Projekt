7b69b75c12067f0009285ae53e37e483552805a2
adamrasin <113976679+adamrasin@users.noreply.github.com>
2024-04-15 16:59:04 +0200

    Verze 2
    Mělo by fungovat ovládání, jako další věc budu muset zfinalizovat skiny, protože jsou nezbytné pro další vývoj.

--- Pac Man Game/Pac Man Game/Form1.cs
+++ Pac Man Game/Pac Man Game/Form1.cs
@@ -18,6 +18,7 @@ namespace Pac_Man_Game
         public Form1()
         {
             InitializeComponent();
+            resetGame();
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -36,25 +37,85 @@ namespace Pac_Man_Game
         }
 
         private void keyisdown(object sender, KeyEventArgs e)
-        {
-
+        { 
+            if (e.KeyCode == Keys.Up)
+            {
+                goup = true;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                godown = true;
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                goleft = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goright = true;
+            }
         }
 
         private void keyisup(object sender, KeyEventArgs e)
         {
-
+            if (e.KeyCode == Keys.Up)
+            {
+                goup = false;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                godown = false;
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                goleft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goright = false;
+            }
         }
 
         private void resetGame()
         {
             score = 0;
             txtScore.Text = "Score: 0";
+            playerSpeed = 8;
             isGameOver = false;
         }
 
         private void mainGameTimer(object sender, EventArgs e)
         {
+            txtScore.Text = "Score: " + score;
+            if (goleft == true)
+            {
+               
+            }
+            if (goright == true)
+            {
 
+            }
+            if (godown == true)
+            {
+
+            }
+            if (goup == true)
+            {
+
+            }
+
+
+
+            foreach(Control x in this.Controls)
+            {
+                if (x is PictureBox)
+                {
+                    if ((string)x.Tag == "coin" && x.Visible == true)
+                    {
+                       
+                    }
+                }
+            }
         }
     }
 }
